fc850892ce4219f1d54a04873133b50e9626a212
afish <yzyafish@gmail.com>
2020-09-17 22:05:45 +0800

    init

---
+++ src/views/swiper/index.js
@@ -0,0 +1,13 @@
+// 引入 html 模板，会被作为字符串引入
+import template from './index.html'
+
+// 引入 css, 会生成 <style> 块插入到 <head> 头中
+import './style.scss'
+
+// 导出类
+export default class {
+  mount(container) {
+    document.title = 'foo'
+    container.innerHTML = template
+  }
+}
