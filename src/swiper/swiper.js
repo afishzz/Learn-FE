fe0738cc493f0600e1a450791ea368ce437b783a
FishWarior <yzyafish@gmail.com>
2020-09-21 09:59:17 +0800

    完成前后移动

--- src/swiper/swiper.js
+++ src/swiper/swiper.js
@@ -6,13 +6,17 @@ class Swiper {
 
     this.swiper = document.getElementsByClassName('swiper-normal')[0]
     this.step = this.swiper.offsetWidth
+
+    this.mainView = null
+
+    this.currIndex = 0
   }
 
   init() {
-    this.createContainer()
+    this._createContainer()
   }
 
-  createContainer() {
+  _createContainer() {
     const { step, retImgList } = this
     // 轮播图片dom
     const mainView = document.createElement('ul')
@@ -38,20 +42,49 @@ class Swiper {
       prev.style.display = 'none'
       next.style.display = 'none'
     }
+    prev.addEventListener('click', e => {
+      e.stopPropagation()
+      this._prev()
+    })
+    next.addEventListener('click', e => {
+      e.stopPropagation()
+      this._next()
+    })
 
     let fragment = document.createDocumentFragment()
     fragment.appendChild(prev)
     fragment.appendChild(next)
     fragment.appendChild(mainView)
     this.swiper.appendChild(fragment)
+
+    this.mainView = mainView
   }
 
-  prev() {
-    
+  _prev() {
+    if (this.imgList.length === 1) return
+    this.mainView.style.left = `${parseInt(this.mainView.style.left) + this.step}px`
+    if (this.currIndex === 0) {
+      this.currIndex = this.imgList.length - 1
+      setTimeout(() => {
+        this.mainView.style.left = `${-this.imgList.length * this.step}px`
+      }, 1000)
+    } else {
+      this.currIndex--
+    }
   }
 
-  next() {
-
+  _next() {
+    if (this.imgList.length === 1) return;
+    this.currIndex++;
+    this.mainView.style.left = `${parseInt(this.mainView.style.left) - this.step}px`;
+    if (this.currIndex === this.imgList.length) {
+      this.currIndex = 0;
+      setTimeout(() => {
+        this.mainView.style.left = `${-this.step}px`;
+      }, 1000)
+    } else {
+      // this.setActiveSpot();
+    }
   }
 
 }
